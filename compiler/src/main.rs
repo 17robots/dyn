f4dca3a5eb3aaed38cf56a9a62fb135fdd5cacfe
17robots <mdray@ameritech.net>
2023-09-19 23:46:44 -0400

    fix: rewrite token algo

--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -83,26 +83,18 @@ static KEYWORDS: Map<&str, TokenType> = phf_map! {
 
 static OPERATORS: Map<&str, TokenType> = phf_map! {
     "+" => TokenType::Add,
-    "++" => TokenType::Inc,
     "-" => TokenType::Sub,
-    "--" => TokenType::Dec,
     "*" => TokenType::Mul,
     "/" => TokenType::Div,
     "%" => TokenType::Per,
     ";" => TokenType::Semicolon,
     ":" => TokenType::Colon,
     "=" => TokenType::Equal,
-    "==" => TokenType::Eq,
     "&" => TokenType::And,
-    "&&" => TokenType::LAnd,
     "|" => TokenType::Or,
-    "||" => TokenType::LOr,
     "!" => TokenType::Not,
-    "!=" => TokenType::Neq,
     "<" => TokenType::Lt,
-    "<=" => TokenType::Lte,
     ">" => TokenType::Gt,
-    ">=" => TokenType::Gte,
     "(" => TokenType::LParen,
     "[" => TokenType::LBrace,
     "{" => TokenType::LBrack,
@@ -111,138 +103,156 @@ static OPERATORS: Map<&str, TokenType> = phf_map! {
     "}" => TokenType::RBrack,
 };
 
+static COMPOUND_OPERATORS: Map<&str, TokenType> = phf_map! {
+    "--" => TokenType::Dec,
+    "++" => TokenType::Inc,
+    "==" => TokenType::Eq,
+    "!=" => TokenType::Neq,
+    ">=" => TokenType::Gte,
+    "<=" => TokenType::Lte,
+    "&&" => TokenType::LAnd,
+    "||" => TokenType::LOr,
+};
+
+static SEPARATORS: Map<&str, TokenType> = phf_map! {
+    ";" => TokenType::Semicolon,
+    "," => TokenType::Comma,
+};
+
 fn main() {
     // tokenizer
-    let tokens = get_tokens(
-        r#"void main() {
-            io.println("hello world");
-        }"#,
-    );
+    let tokens = get_tokens(r#"123.45"#);
     tokens.iter().for_each(|x| {
         println!("{:?}", x);
     });
 }
 
 fn get_tokens(stream: &str) -> Vec<TokenType> {
+    let mut curr_pos: u32 = 0;
+    let mut offset: i32 = -1;
     let mut tokens = Vec::new();
-    let mut buff = String::from("");
 
-    if stream.len() == 0 {
-        return tokens;
-    }
-    stream
-        .replace("\n", " ")
-        .split(' ')
-        .map(|z| z.trim())
-        .for_each(|x| {
-            if x.len() > 1 {
-                x.chars().for_each(|y| match y {
-                    '0'..='9' | 'a'..='z' | 'A'..='Z' => {
-                        buff.push(y);
-                    }
-                    '.' => {
-                        if buff.contains('.') {}
-                        if let Ok(_) = buff.parse::<i128>() {
-                            buff.push(y);
-                        } else {
-                            tokens.push(TokenType::Identifier(buff.clone()));
-                            buff.clear();
-                            tokens.push(TokenType::Period);
-                        }
-                    }
-                    ',' => {
-                        if buff.len() > 0 {
-                            tokens.push(TokenType::Identifier(buff.clone()));
-                            buff.clear();
-                        }
-                        tokens.push(TokenType::Comma);
-                    }
-                    '\"' => {
-                        if buff.contains(y) {
-                            buff.remove(0); // remove matching "
-                            tokens.push(TokenType::StringLiteral(buff.clone()));
-                            buff.clear();
-                        } else {
-                            if buff.len() > 0 {
-                                // handle error
-                            } else {
-                                buff.push(y);
-                            }
-                        }
-                    }
-                    '\'' => {
-                        if buff.contains(y) {
-                            // pop buff out and store as char literal
-                            if buff.len() > 2 {
-                                // handle error
-                            } else {
-                                buff.remove(0);
-                                tokens.push(TokenType::Character(buff.chars().last().unwrap()));
-                                buff.clear();
-                            }
-                        } else {
-                            if buff.len() > 0 {
-                                // handle error
-                            } else {
-                                buff.push(y);
-                            }
-                        }
-                    }
-                    '(' | '[' | '{' | ')' | ']' | '}' => {
-                        if buff.len() > 0 {
-                            // could be fn call or the end of an arg list
-                            tokens.push(TokenType::Identifier(buff.clone()));
-                            buff.clear();
-                        }
-                        tokens.push(OPERATORS.get(&y.to_string()).unwrap().clone());
-                    }
-                    ';' => {
-                        if buff.len() > 0 {
-                            let mut buff_tokens = get_tokens(&buff);
-                            tokens.append(&mut buff_tokens);
-                            buff.clear();
-                        }
-                        tokens.push(TokenType::Semicolon);
-                    }
-                    _ => {}
-                });
-                if buff.len() > 0 {
-                    // we still have stuff left over
-                    if KEYWORDS.contains_key(&buff) {
-                        tokens.push(KEYWORDS.get(&buff).unwrap().clone());
-                    } else {
-                        if let Ok(num) = buff.parse::<i128>() {
-                            tokens.push(TokenType::Integer(num));
-                        } else if let Ok(num) = buff.parse::<f64>() {
-                            tokens.push(TokenType::Floating(num));
-                        } else {
-                            tokens.push(TokenType::Identifier(buff.clone()));
-                        }
-                    }
-                    buff.clear();
-                }
-            } else {
-                if x.is_empty() {
-                    return;
-                }
-                if let Some(token) = get_token_from_char(&x.chars().next().unwrap()) {
-                    tokens.push(token);
+    loop {
+        if curr_pos as usize >= stream.len() {
+            break;
+        }
+
+        let ch = &stream.chars().nth(curr_pos as usize).unwrap();
+
+        match ch {
+            'a'..='z' | 'A'..='Z' | '0'..='9' => {
+                if offset < 0 {
+                    offset = curr_pos as i32;
                 } else {
-                    // TODO: handle this as an error later
+                    let buff = &stream[offset as usize..curr_pos as usize];
+                    match buff {
+                        _ if COMPOUND_OPERATORS.contains_key(&buff) => {
+                            tokens.push(COMPOUND_OPERATORS.get(&buff).unwrap().clone());
+                            offset = curr_pos as i32;
+                        }
+                        _ if OPERATORS.contains_key(&buff) => {
+                            tokens.push(OPERATORS.get(&buff).unwrap().clone());
+                            offset = curr_pos as i32;
+                        }
+                        _ => {} // nothing that I care about
+                    }
                 }
             }
-        });
-    tokens.append(&mut get_tokens(&buff));
+            _ => {
+                let mut s = String::from("");
+                if offset > -1 {
+                    let buff = &stream[offset as usize..curr_pos as usize];
+                    match buff {
+                        _ if COMPOUND_OPERATORS
+                            .contains_key(&format!("{}{}", buff, ch).as_str()) =>
+                        {
+                            tokens.push(
+                                COMPOUND_OPERATORS
+                                    .get(&format!("{}{}", buff, ch).as_str())
+                                    .unwrap()
+                                    .clone(),
+                            );
+                        }
+                        _ if OPERATORS.contains_key(&buff) => {
+                            tokens.push(OPERATORS.get(&buff).unwrap().clone());
+                        }
+                        _ if KEYWORDS.contains_key(&buff) => {
+                            tokens.push(KEYWORDS.get(&buff).unwrap().clone())
+                        }
+
+                        _ if is_number(buff) => s = buff.to_string(), // dont do anything
+                        _ => tokens.push(TokenType::Identifier(buff.to_owned().clone())),
+                    }
+                    offset = if is_number(buff) { offset } else { -1 }; // keep offset for now since its a number
+                }
+                match ch {
+                    _ if SEPARATORS.contains_key(&ch.to_string()) => {
+                        if offset > -1 {
+                            tokens.push(if s.contains('.') {
+                                TokenType::Floating(s.parse::<f64>().unwrap())
+                            } else {
+                                TokenType::Integer(s.parse::<i128>().unwrap())
+                            });
+                        }
+                        offset = -1;
+                        tokens.push(SEPARATORS.get(&ch.to_string()).unwrap().clone());
+                    }
+                    '.' => {
+                        if offset > -1 {
+                            if s.contains('.') {
+                                tokens.push(TokenType::Floating(s.parse::<f64>().unwrap()));
+                                tokens.push(TokenType::Period);
+                            }
+                        } else {
+                            tokens.push(TokenType::Period);
+                        }
+                    } // this needs to also factor in numbers but again Im too lazy rn
+                    _ if OPERATORS.contains_key(&ch.to_string()) => {
+                        if offset > -1 {
+                            tokens.push(if s.contains('.') {
+                                TokenType::Floating(s.parse::<f64>().unwrap())
+                            } else {
+                                TokenType::Integer(s.parse::<i128>().unwrap())
+                            });
+                        }
+                        offset = curr_pos as i32; // move into buffer in case theres compound operator
+                    }
+                    _ => {} // ignore spaces and other stuff for now because I just dont care yet thisll also factor in other stuff
+                }
+            }
+        }
+
+        curr_pos += 1;
+    }
+    if offset > -1 {
+        let buff = &stream[offset as usize..curr_pos as usize];
+        match buff {
+            _ if COMPOUND_OPERATORS.contains_key(&buff) => {
+                tokens.push(COMPOUND_OPERATORS.get(&buff).unwrap().clone());
+            }
+            _ if OPERATORS.contains_key(&buff) => {
+                tokens.push(OPERATORS.get(&buff).unwrap().clone());
+            }
+            _ if KEYWORDS.contains_key(&buff) => tokens.push(KEYWORDS.get(&buff).unwrap().clone()),
+            _ if SEPARATORS.contains_key(&buff) => {
+                tokens.push(SEPARATORS.get(&buff).unwrap().clone())
+            }
+            "." => tokens.push(TokenType::Period),
+            _ if is_number(&buff) => tokens.push(if buff.contains('.') {
+                TokenType::Floating(buff.parse::<f64>().unwrap())
+            } else {
+                TokenType::Integer(buff.parse::<i128>().unwrap())
+            }),
+            _ => tokens.push(TokenType::Identifier(buff.to_owned().clone())),
+        }
+    }
     tokens
 }
 
-fn get_token_from_char(x: &char) -> Option<TokenType> {
-    match x {
-        'a'..='z' | 'A'..='Z' => Some(TokenType::Identifier(x.to_string().clone())),
-        '0'..='9' => Some(TokenType::Integer(x.to_string().parse::<i128>().unwrap())),
-        _ if OPERATORS.contains_key(&x.to_string()) => {
-            Some(OPERATORS.get(&x.to_string()).unwrap().clone())
-        }
-        _ => None,
+fn is_number(s: &str) -> bool {
+    if let Ok(_) = s.parse::<f64>() {
+        true
+    } else {
+        false
     }
 }
