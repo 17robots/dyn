d5af3b8c8092998c5770ec16afc5da5f2d9b3dee
17robots <mdray@ameritech.net>
2023-10-05 23:42:32 -0400

    fix: rewrite tokenizer

--- compiler/src/main.zig
+++ compiler/src/main.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 
 const string = []const u8;
-const tokenArr = std.ArrayList(Token);
 const language = [_]string{
     "con",
     "mut",
@@ -39,7 +38,7 @@ const language = [_]string{
 fn contains(val: string) bool {
     var found = false;
     for (language) |x| {
-        if (!std.mem.eql(u8, x, val)) {
+        if (std.mem.eql(u8, x, val)) {
             found = true;
         }
     }
@@ -47,124 +46,124 @@ fn contains(val: string) bool {
 }
 
 const File = struct {
-    input: string,
     name: string,
-    pos: usize,
-    ch: ?u8,
-    fn new(name: string, body: string) File {
-        var f: File = .{ .name = name, .pos = 0, .ch = undefined, .input = body };
+    input: string,
+    curr: usize,
+    next: usize,
+    ch: u8,
+    fn new(name: string, input: string) File {
+        var f: File = .{
+            .name = name,
+            .input = input,
+            .curr = 0,
+            .next = 0,
+            .ch = 0,
+        };
         f.read_char();
         return f;
     }
-    fn next_token(self: *File) Token {
-        var tok: Token = Token.new_empty();
-        if (self.ch == undefined) {
-            tok = Token.new("EOF", "", self.pos, self.pos);
-        }
-        if (self.is_letter()) {
-            var x = self.read_word();
-            var y: string = "";
-            if (contains(x)) {
-                y = "Keyword";
-            } else {
-                y = "Identifier";
-            }
-            tok = Token.new(y, x, self.pos - x.len, self.pos);
-        } else if (self.is_number()) {
-            var x = self.read_number();
-            var y: string = "";
-            if (std.mem.count(u8, x, ".") > 0) {
-                y = "Floating";
-            } else {
-                y = "Integer";
-            }
-            tok = Token.new(y, x, self.pos - x.len, self.pos);
-        } else if (self.is_operator()) {
-            const x = self.peak();
-            const y: string = &[_]u8{self.ch.?} ++ &[_]u8{x};
-            if (x != undefined and contains(y)) {
-                self.read_char(); // eat peaked character
-                tok = Token.new("Operator", y, self.pos - 1, self.pos);
-            } else {
-                tok = Token.new("Operator", &[_]u8{self.ch.?}, self.pos, self.pos);
-            }
-        } else if (self.is_whitespace()) {
-            tok = Token.new("Whitespace", "", self.pos, self.pos);
-        }
-        if (std.mem.eql(u8, tok.type, "")) {
-            tok = Token.new("Illegal", "", self.pos, self.pos);
-        }
-        self.read_char(); // move to the next char before returning
-        return tok;
-    }
     fn read_char(self: *File) void {
-        self.ch = undefined;
-        if (self.pos <= self.input.len) {
-            self.ch = self.from_stream(self.pos, self.pos)[0];
-        }
-        self.pos += 1;
+        self.ch = self.peak();
+        self.curr = self.next;
+        self.next += 1;
     }
     fn peak(self: *File) u8 {
-        const peak_pos = self.pos + 1;
-        var c: u8 = undefined;
-        if (peak_pos <= self.input.len) {
-            c = self.from_stream(peak_pos, peak_pos)[0];
+        if (self.next < self.input.len) {
+            return self.input[self.next];
         }
-        return c;
+        return 0;
+    }
+    fn get_token(self: *File) Token {
+        const pos = self.curr;
+        var tok: Token = undefined;
+        self.read_ws();
+        switch (self.ch) {
+            'a'...'z', 'A'...'Z' => {
+                var x = self.read_word();
+                var y: string = undefined;
+                if (contains(x)) {
+                    y = "Keyword"; // keyword
+                } else {
+                    y = "Identifer"; // identifier
+                }
+                tok = Token.new(y, x, pos, self.curr - 1);
+            },
+            '0'...'9' => {
+                var x = self.read_num();
+                var y: string = undefined;
+                if (std.mem.count(u8, x, ".") > 0) {
+                    y = "Floating"; // float
+                } else {
+                    y = "Integer"; // integer
+                }
+                tok = Token.new(y, x, pos, self.curr - 1); // int
+            },
+            else => {
+                if (contains(&[_]u8{self.ch})) {
+                    const x = self.read_op();
+                    tok = Token.new("Operator", x, pos, self.curr - 1);
+                } else if (self.next >= self.input.len) {
+                    tok = Token.new("EOF", "", pos, self.curr - 1); // eof
+                } else {
+                    tok = Token.new("Illegal", "", pos, self.curr - 1); // illegal
+                }
+            },
+        }
+        if (self.curr == pos)
+            self.read_char();
+        return tok;
+    }
+    fn read_num(self: *File) string {
+        const pos = self.curr;
+        blk: while (true) {
+            switch (self.ch) {
+                '0'...'9', '.', '_' => {
+                    self.read_char();
+                },
+                else => {
+                    break :blk;
+                },
+            }
+        }
+        return self.input[pos..self.curr];
     }
     fn read_word(self: *File) string {
-        const pos = self.pos;
-        while (self.is_letter() or self.is_number()) {
-            self.read_char();
+        const pos = self.curr;
+        blk: while (true) {
+            switch (self.ch) {
+                'a'...'z', 'A'...'Z', '0'...'9', '_' => {
+                    self.read_char();
+                },
+                else => {
+                    break :blk;
+                },
+            }
         }
-        return self.from_stream(pos, self.pos);
+        return self.input[pos..self.curr];
     }
-    fn read_number(self: *File) string {
-        const pos = self.pos;
-        while (self.is_number()) {
+    fn read_op(self: *File) string {
+        const pos = self.curr;
+        blk: while (contains(&[_]u8{self.ch})) {
             self.read_char();
+            if (!contains(self.input[pos..self.curr])) {
+                break :blk;
+            }
         }
-        return self.from_stream(pos, self.pos);
+        return self.input[pos..self.curr];
     }
-    fn is_letter(self: *File) bool {
-        return switch (self.ch.?) {
-            'a'...'z', 'A'...'Z' => {
-                return true;
-            },
-            else => {
-                return false;
-            },
-        };
-    }
-    fn is_number(self: *File) bool {
-        return switch (self.ch.?) {
-            '0'...'9', '_', '.' => {
-                return true;
-            },
-            else => {
-                return false;
-            },
-        };
-    }
-    fn is_operator(self: *File) bool {
-        return contains(&[_]u8{self.ch.?});
-    }
-    fn is_whitespace(self: *File) bool {
-        return switch (self.ch.?) {
-            ' ',
-            '\t',
-            '\n',
-            '\r',
-            => {
-                return true;
-            },
-            else => {
-                return false;
-            },
-        };
-    }
-    fn from_stream(self: *File, start: usize, end: usize) string {
-        return self.input[start..(end + @intFromBool(end == start))];
+    fn read_ws(self: *File) void {
+        blk: {
+            while (true) {
+                switch (self.ch) {
+                    ' ', '\t', '\r' => {
+                        self.read_char();
+                    },
+                    else => {
+                        break :blk;
+                    },
+                }
+            }
+        }
     }
 };
 const Token = struct {
@@ -172,48 +171,26 @@ const Token = struct {
     val: string,
     start: usize,
     end: usize,
-    fn new(_type: string, val: string, start: usize, end: usize) Token {
+    fn new(_type: string, val: string, s: usize, e: usize) Token {
         return .{
             .type = _type,
             .val = val,
-            .start = start,
-            .end = end,
-        };
-    }
-    fn new_empty() Token {
-        return .{
-            .type = "",
-            .val = undefined,
-            .start = 0,
-            .end = 0,
-        };
-    }
-    fn print(self: *Token) void {
-        _ = std.io.getStdOut().writer().print("{s}({s}), start: {d}, end: {d}\n", .{ self.type, self.val, self.start, self.end }) catch false;
-    }
-};
-const Lexer = struct {
-    file: File,
-    tokens: tokenArr,
-    fn new(alloc: std.mem.Allocator, file: File) Lexer {
-        return .{
-            .file = file,
-            .tokens = std.ArrayList(Token).init(alloc),
+            .start = s,
+            .end = e,
         };
     }
 };
+
+pub fn print_token(t: Token) void {
+    std.debug.print("{s}({s}), start: {d}, end: {d}\n", .{ t.type, t.val, t.start, t.end });
+}
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    var alloc = gpa.allocator();
-    var file = File.new("main.dyn", "i32 x = 4;");
-    var lexer = Lexer.new(alloc, file);
-    defer lexer.tokens.deinit();
-
-    var token = lexer.file.next_token();
-    while (!std.mem.eql(u8, token.type, "EOF")) {
-        token.print();
-        token = lexer.file.next_token();
+    var f = File.new("main.dyn", "f32 x = 4.0;");
+    var token = f.get_token();
+    while (std.mem.count(u8, token.type, "EOF") == 0) {
+        print_token(token);
+        token = f.get_token();
     }
     return;
 }
