f5904b49c1fcb3471c2b5859af7259e98218f6d6
17robots <mdray@ameritech.net>
2023-11-09 00:27:09 -0500

    feat: trying something new in the lexer

--- compiler/src/lexer2.zig
+++ compiler/src/lexer2.zig
@@ -7,8 +7,62 @@ pub const File = struct {
 
 pub const Tag = enum {
     // general / literals
+    invalid,
+    eof,
+    num_literal,
+    string_literal,
+    char_literal,
     // symbols
+    plus,
+    plus_plus,
+    plus_equal,
+    minus,
+    minus_minus,
+    plus_equal,
+    asterisk,
+    asterisk_equal,
+    slash,
+    slash_equal,
+    percent,
+    percent_equal,
+    equal,
+    equal_equal,
+    bang,
+    bang_equal,
+    ampersand,
+    ampersand_ampersand,
+    ampersand_equal,
+    pipe,
+    pipe_pipe,
+    pipe_equal,
+    less_than,
+    less_than_equal,
+    greater_than,
+    greater_than_equal,
+    l_paren,
+    r_paren,
+    l_bracket,
+    r_bracket,
+    l_brace,
+    r_brace,
+    period,
+    comma,
+    colon,
+    semicolon,
     // keywords
+    keyword_if,
+    keyword_else,
+    keyword_struct,
+    keyword_loop,
+    keyword_true,
+    keyword_false,
+    keyword_import,
+    keyword_pub,
+    keyword_con,
+    keyword_mut,
+    keyword_type,
+    keyword_trait,
+    keyword_enum,
 };
 
 pub const Loc = struct {
@@ -24,6 +78,11 @@ pub const Token = struct {
 
 pub const Tokenizer = struct {
     file: *File,
+    state: ReadStates,
+    index: usize,
+
+    pub fn init() Tokenizer {}
+
     pub const ReadStates = enum {
         start,
         identifier,
@@ -31,4 +90,129 @@ pub const Tokenizer = struct {
         literal_string,
         literal_char,
     };
+
+    pub fn next(self: *Tokenizer) Token {
+        const c = self.file.buf[self.index];
+        var tok: Token = .{ .loc = .{
+            .file = self.file,
+            .start = 0,
+            .end = undefined,
+        }, .tag = undefined };
+        switch (c) {
+            '0'...'9' => switch (self.state) {},
+            'a'...'z', 'A'...'Z', '_' => switch (self.state) {},
+            '"' => switch (self.state) {
+                .start => {},
+                .string_literal => {},
+                else => {},
+            },
+            '\\' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '\'' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '<' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '>' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '=' => switch (self.state) {
+                .start => {},
+                .ampersand => {},
+                .asterisk => {},
+                .percent => {},
+                .plus => {},
+                .minus => {},
+                .bang => {},
+                .pipe => {},
+                .equal => {},
+                else => {
+                    tok.tag = .equal;
+                },
+            },
+            '+' => switch (self.state) {
+                .start => {},
+                .plus => {},
+                else => {},
+            },
+            '-' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '/' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '*' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '|' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '&' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '!' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '?' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            ',' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            ';' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            ':' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '(' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            ')' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '[' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            ']' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '{' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            '}' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            ' ', '\n', '\r' => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+            else => switch (self.state) {
+                .start => {},
+                else => {},
+            },
+        }
+    }
 };
