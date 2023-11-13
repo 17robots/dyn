9edf2e612db0a3ff168a5cb64b7210752a011053
17robots <mdray@ameritech.net>
2023-11-13 18:03:50 -0500

    feat: switch to cpp

---
+++ compiler/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "TutorialConfig.h"
+
+int main() {
+    std::cout << "Hello World\n";
+    std::cout << Tutorial_VERSION_MINOR << '.' << Tutorial_VERSION_MAJOR << '\n';
+    return 0;
+}
