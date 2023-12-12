bb5b4a32a6d8f1726ae61373abcd699943f0275d
17robots <mdray@ameritech.net>
2023-12-12 00:06:02 -0500

    feat: finish basic lexing algo

--- compiler/src/main2.cc
+++ compiler/src/main2.cc
@@ -31,8 +31,8 @@ enum TokenType {
   PIPE_EQUAL,
   LT,
   GT,
-  GT_GT,
-  LT_LT,
+  GT_EQUAL,
+  LT_EQUAL,
   L_PAREN,
   L_BRACK,
   L_BRACE,
@@ -62,6 +62,39 @@ std::unordered_map<std::string, TokenType> kwds = {
     {"from", FROM},   {"import", IMPORT},
 };
 
+std::unordered_map<std::string, TokenType> operators = {
+    {"(", L_PAREN},
+    {"[", L_BRACK},
+    {"{", L_BRACE},
+    {")", R_PAREN},
+    {"]", R_BRACK},
+    {"}", R_BRACE},
+    {";", SEMICOLON},
+    {":", COLON},
+    {"~", TILDE},
+    {"\'", SINGLE_QUOTE},
+    {"\"", DOUBLE_QUOTE},
+    {"=", EQUAL},
+    {"==", EQUAL_EQUAL},
+    {"+", PLUS},
+    {"+=", PLUS_EQUAL},
+    {"-", MINUS},
+    {"-=", MINUS_EQUAL},
+    {"*", ASTERISK},
+    {"*=", ASTERISK_EQUAL},
+    {"/", SLASH},
+    {"/=", SLASH_EQUAL},
+    {"&", AMPERSAND},
+    {"&&", AMPERSAND_AMPERSAND},
+    {"&=", AMPERSAND_EQUAL},
+    {"|", PIPE},
+    {"||", PIPE_PIPE},
+    {"|=", PIPE_EQUAL},
+    {"<", LT},
+    {">", GT},
+    {">=", GT_EQUAL},
+    {"<=", LT_EQUAL}};
+
 struct Token {
   TokenType t;
   std::string v;
@@ -78,6 +111,7 @@ enum TokenState {
 };
 
 bool is_l(char c) { return (c > 40 && c < 91) || (c > 60 && c < 123); }
+bool is_w(char c) { return c == ' ' || c == '\n' || c == '\t'; }
 bool is_n(char c, TokenState s) {
   return (c == '.' || c == '_') ? s == READ_NUM : (c < 47 || c < 58);
 }
@@ -103,15 +137,44 @@ Token next(std::string input, int &out) {
       if (is_l(x)) {
         buf.push_back(x);
         s = READ_WORD;
-      };
-      if (is_n(x, s)) {
+      } else if (is_n(x, s)) {
         buf.push_back(x);
         s = READ_NUM;
-      };
-      if (is_o(x)) {
-        buf.push_back(x);
-        s = READ_OP;
-      };
+      } else if (is_o(x)) {
+        switch (x) {
+        case '\"':
+        case '\'':
+        case '=':
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '&':
+        case '|':
+        case '>':
+        case '<':
+        case '.': // because . could be a number
+          buf.push_back(x);
+          s = READ_OP;
+          break;
+        case '(':
+        case '[':
+        case '{':
+        case ')':
+        case ']':
+        case '}':
+        case ';':
+        case ':':
+          t.t = operators.at(std::string(1, x));
+          t.v = "";
+          break;
+        }
+      } else if (is_w(x)) {
+      } else {
+        t.t = ILLEGAL;
+        t.v = "";
+        return t;
+      }
     case READ_WORD:
       if (is_l(x) || is_n(x, s)) {
         buf.push_back(x);
@@ -126,20 +189,41 @@ Token next(std::string input, int &out) {
       if (buf.size() > 0 && x != '\'') {
         t.t = ILLEGAL;
         t.v = "";
-        s = START;
       } else if (buf.size() > 0 && x == '\'') {
-        auto y = pop_buf(buf);
+        auto y = pop_buf(buf, s);
         t.t = CHAR;
         t.v = y;
-        s = START;
       } else {
         buf.push_back(x);
         break;
       }
     case READ_NUM:
-      if (is_l(x) || is_o(x)) {
+      if (is_n(x, s)) {
+      } else {
+        auto y = pop_buf(buf, s);
+        t.v = y;
+        t.t = y.find('.') != std::string::npos ? FLOAT : INT;
+        return t;
       }
-    case READ_OP:
+    case READ_OP: {
+      if(is_n(x, s)) {
+          if(buf.at(0) == '.') {
+            buf.push_back(x);
+            s = READ_NUM;
+            break;
+          }
+        }
+      if (is_o(x)) {
+        std::string c = std::string(buf.begin(), buf.end()) + std::string(1, x);
+        if (operators.find(c) != operators.end()) {
+          buf.push_back(x);
+        }
+      }
+      auto y = pop_buf(buf, s);
+      t.v = "";
+      t.t = operators.at(y);
+      return t;
+    }
     case READ_STRING:
       break;
     default:
@@ -157,7 +241,7 @@ std::vector<Token> tokenize(std::string input) {
   do {
     tok = next(input, offset);
     t.push_back(tok);
-  } while (tok.t == EOF);
+  } while (tok.t != EOF);
   return t;
 }
 
