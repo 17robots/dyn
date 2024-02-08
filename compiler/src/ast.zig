ab63696ba4f9d0b29d2bb12a8329efb02cacfc79
17robots <mdray@ameritech.net>
2024-02-07 23:21:32 -0500

    feat: add fns for node types

--- compiler/src/ast.zig
+++ compiler/src/ast.zig
@@ -1,28 +1,28 @@
 const std = @import("std");
-const Token = @import("lexer.zig").Token;
+const l = @import("lexer.zig");
 pub const Ast = struct {
     allocator: std.mem.Allocator,
-    sources: std.ArrayList([]const u8),
-    tokens: std.ArrayList(Token),
-    token_source_map: std.StringHashMap(std.ArrayList(Token)),
+    source: []const u8,
+    imports: std.StringHashMap(std.StringHashMap([]const u8)),
+    tokens: std.ArrayList(l.Token),
+    token_source_map: std.StringHashMap(std.ArrayList(l.Token)),
     const ParsingState = enum {
         start,
     };
-    pub fn init(alloc: std.mem.Allocator, filenames: [][]const u8) !Ast {
-        var ast = Ast{ .alloc = alloc, .sources = std.ArrayList([]const u8).init(alloc), .tokens = undefined };
-        for (filenames) |name| {
-            try ast.sources.append(name);
-        }
-        return ast;
+    pub fn init(alloc: std.mem.Allocator, filename: []const u8) !Ast {
+        return .{ .alloc = alloc, .source = filename, .tokens = undefined, .imports = std.StringHashMap(std.StringHashMap([]const u8).init(alloc)) };
     }
     pub fn deinit(self: *Ast) void {
-        defer self.sources.deinit();
         defer self.tokens.deinit();
+        defer self.imports.deinit();
+        for (self.imports.valueIterator().items) |i| {
+            for (i.valueIterator().items) |j| {
+                j.deinit();
+            }
+            i.deinit();
+        }
     }
-    pub fn add_source(self: *Ast, file: []const u8) !void {
-        try self.sources.append(file);
-    }
-    fn tokenize_source(self: *Ast) !void {
+    fn tokenize(self: *Ast) !void {
         _ = self;
     }
 };
@@ -44,5 +44,166 @@ const Node = struct {
     pub fn deinit(self: *Node) void {
         defer self.children.deinit();
         defer self.attrs.deinit();
+    }
+
+    // root
+    pub fn program(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    // declaration nodes
+    pub fn declaration(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    } // pub?
+    pub fn variable_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    } // mut?
+    pub fn struct_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn struct_variable_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn struct_function_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn enum_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn enum_member_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn function_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn function_arg_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn type_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn import_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    // statements
+    pub fn statement(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn if_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn loop_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn for_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn match_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn match_branc_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn defer_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn break_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn continue_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    // expressions
+    pub fn expression(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn identifier_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn integer_literal_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn float_literal_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn boolean_literal_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn string_literal_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn char_literal_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn function_call_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn unary_expr_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn binary_expr_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn paren_expr_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn anon_function_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn anon_struct_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn anon_enum_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn type_literal_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn array_type_literal_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn pointer_type_literal_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
+    }
+    pub fn reference_type_literal_node(toks: *std.ArrayList(l.Token), index: u32) !Node {
+        _ = index;
+        _ = toks;
     }
 };
