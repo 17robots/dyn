21b0349bf664f60f62873f4fa1b7b8b1b9b44aa9
17robots <mdray@duck.com>
2024-08-25 12:01:05 -0400

    fix: underscore

---
+++ src/token.zig
@@ -0,0 +1,78 @@
+pub const TokenType = enum {
+    eof,
+    invalid,
+    // literals
+    identifier,
+    int,
+    float,
+    string,
+    char,
+    // operators
+    add, // +
+    sub, // -
+    mul, // *
+    div, // /
+    mod, // %
+    @"and", // &
+    @"or", // |
+    xor, // ^
+    flip, // ~
+    eq, // =
+    addeq, // +=
+    addadd, // ++
+    subeq, // -=
+    subsub, // --
+    muleq, // *=
+    diveq, // /=
+    modeq, // %=
+    andeq, // &=
+    oreq, // |=
+    xoreq, // ^=
+    flipeq, // ~=
+    andand, // &&
+    oror, // ||
+    eqeq, // ==
+    gt, // >
+    lt, // <
+    gteq, // >=
+    lteq, // <=
+    lparen, // (
+    rparen, // )
+    lbrack, // [
+    rbrack, // ]
+    lbrace, // {
+    rbrace, // }
+    dot, // .
+    dotdot, // ..
+    colon, // :
+    semicolon, // ;
+    underscore, // _
+    comma, // ,
+    arrow, // =>
+    bang, // !
+    bangeq, // !=
+    question, // ?
+    dollar, // $
+    // keywords
+    module,
+    use,
+    void,
+    mut,
+    true,
+    false,
+    @"if",
+    @"else",
+    match,
+    @"defer",
+    loop,
+    @"for",
+    @"enum",
+    @"error",
+    @"try",
+    @"catch",
+    @"union",
+    @"struct",
+    type,
+    comp,
+};
+
