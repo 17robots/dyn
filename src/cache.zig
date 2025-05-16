8323a81731cdb3c580c8637d5195ff0ef15162ce
17robots <mdray@duck.com>
2025-05-16 17:46:56 -0400

    feat: cache changes

--- src/cache.zig
+++ src/cache.zig
@@ -1,47 +1,39 @@
 const std = @import("std");
-pub fn Cache(comptime T: type, items: usize) type {
+pub fn Cache(comptime K: type, comptime V: type, items: usize) type {
     return struct {
-        const CacheItem = struct { v: T, dirty: bool = false };
-        const InvalidateFn = *fn (CacheMap) bool;
+        const CacheItem = struct { k: K, v: V, dirty: bool = false };
+        const InvalidateFn = *fn (CacheItem) bool;
         const Self = @This();
         c: [items]CacheItem,
+        i: InvalidateFn,
         fn init() Self {
-            return Self{ .c = [1]CacheItem{.{ .v = undefined, .dirty = false }} ** items };
+            return Self{ .c = [1]CacheItem{.{ .k = undefined, .v = undefined, .dirty = false }} ** items };
         }
-        fn is_cached(s: Self, v: T) bool {
-            for(s.c) |i| { }
+        fn get_index(s: *Self, k: K) ?usize {
+            for (0..s.c.len) |i| {
+                if (s.eq(K, s.c[i], k)) {
+                    if (s.i(s.c[i])) s.c[i].dirty = true;
+                    return if (!s.c[i].dirty) i else null;
+                }
+            }
+            return null;
         }
-    };
-}
-pub fn Cache(comptime T: type, comptime U: type) type {
-    const CacheMap = std.AutoHashMap(T, struct { v: U, dirty: bool = false });
-    const InvalidateFn = *fn (CacheMap) bool;
-    return struct {
-        const Self = @This();
-        c: CacheMap,
-        invalidateFn: InvalidateFn,
-        fn init(alloc: std.mem.Allocator) Self {
-            return Self{ .c = CacheMap.init(alloc) };
+        fn get(s: *Self, k: K) ?CacheItem {
+            for (0..s.c.len) |i| {
+                if (s.eq(K, s.c[i].k, k)) {
+                    if (s.i(s.c[i])) s.c[i].dirty = true;
+                    return if (!s.c[i].dirty) s.c[i] else null;
+                }
+            }
+            return null;
         }
-        fn is_cached(s: *Self, key: T) bool {
-            return if (s.c.get(key)) |*val| blk: {
-                if (s.invalidateFn(val.*)) val.dirty = true;
-                break :blk val.dirty;
-            } else false;
-        }
-        fn mark_dirty(s: *Self) void {
-            while (s.c.valueIterator().next()) |*v| {
-                if (s.invalidateFn(v.*)) v.dirty = true;
+        fn cache(s: *Self, k: K, v: V) void {
+            if (s.get_index(k)) |i| {
+                if(s.eq(V, s.c[i], v)) return; // doesnt need to be cached
             }
         }
-        fn cache(s: *Self, key: T, val: U) !void {
-            if (s.is_cached(key)) return error.AlreadyCached;
-            if (s.c.get(key)) |*v| {
-                v.v = v;
-                v.dirty = false;
-                return;
-            }
-            try s.c.put(key, val);
+        fn eq(comptime T: type, v1: T, v2: T) bool {
+            return std.mem.eql(T, [_]T{v1}, [_]T{v2});
         }
     };
 }
