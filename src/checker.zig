937d7cdb53bcce8d55256a4e639f1af32e29d8a9
17robots <mdray@ameritech.net>
2025-05-30 11:17:57 -0400

    feat: add data structures

--- src/checker.zig
+++ src/checker.zig
@@ -1,13 +1,18 @@
 const std = @import("std");
 const File = @import("file.zig");
-const Module = @import("module.zig");
 
 const Checker = @This();
-const Import = union(enum) {
-    file: File,
-    module: Module
+
+const Module = struct {
+    name: []const u8,
+    files: std.ArrayList(),
+    a: std.mem.Allocator,
+    fn init(a: std.mem.Allocator, name: []const u8) Module {
+        return .{ .name = name, .a = a, .files = std.ArrayList(File).init(a) };
+    }
 };
 
-imports: std.ArrayList(Import),
-files: std.ArrayList(File),
+const Import = union(enum) { file: File, module: Module };
 
+imports: std.ArrayList(Import),
+module: *Module,
