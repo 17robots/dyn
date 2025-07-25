80629c043bdc0bdec92414fd0f5295fddfd039d2
Matthew Dray <mdray@duck.com>
2025-07-25 00:12:44 -0400

    fix: move to new main to start fresh

--- src/main.zig
+++ src/main.zig
@@ -1,16 +1,6 @@
 const std = @import("std");
-const module = @import("module.zig");
-const Checker = @import("checker.zig");
 
 pub fn main() !void {
-    const page_allocator = std.heap.page_allocator;
-    var arena = std.heap.ArenaAllocator.init(page_allocator);
-    defer arena.deinit();
-    const alloc = arena.allocator();
-
-    var checker = try Checker.init(alloc);
-    var mod_resolver = module.ModuleResolver.init(alloc);
-    var a = try mod_resolver.resolveModule(".", "test");
-    try a.parse();
-    try checker.check_program(&a);
+    std.debug.print("Hello World\n", .{});
 }
+
