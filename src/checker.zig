13338a63ade3b2c030f41bafae92d19eb1af500d
17robots <mdray@duck.com>
2025-10-27 20:14:55 -0400

    feat: make global scope part of checker

--- src/checker.zig
+++ src/checker.zig
@@ -8,30 +8,29 @@ const SourceLocation = @import("source.zig").SourceLocation;
 m: *Module,
 d: *DiagnosticEmitter,
 sm: *SourceManager,
-// scopes: symbol.ScopeStack,
+global: *ScopedSymbolTable,
 
 const Checker = @This();
 
 pub fn init(m: *Module, d: *DiagnosticEmitter, sm: *SourceManager) Checker {
-    return .{ .m = m, .d = d, .sm = sm, .scopes = .empty };
+    return .{ .m = m, .d = d, .sm = sm, .scopes = .empty, .global = ScopedSymbolTable.init(m.allocator, .{ .name = "global", .level = 0, .d = d, .parent = null }) };
 }
 pub fn check(s: *Checker) !void {
     var global_scope = ScopedSymbolTable.init(s.m.allocator, .{ .name = "global", .level = 0, .d = s.d, .parent = null });
-    for (s.m.asts.items) |a| s.check_file_globals(a, &global_scope);
+    for (s.m.asts.items, 0..s.m.asts.items.len) |a, i| s.add_file_globals(a, i, &global_scope);
+    for (s.m.asts.items, 0..s.m.asts.items.len) |a, i| s.check_ast(a, i, &global_scope);
 }
-fn check_file_globals(s: *Checker, file_id: u32, ast: Node, scope: *ScopedSymbolTable) void {
+fn add_file_globals(s: *Checker, file_id: u32, ast: Node, scope: *ScopedSymbolTable) void {
     switch (ast.type) {
         .program => |p| {
             for (p.declarations.items) |d| {
                 switch (d.type) {
                     .module => {},
                     .declaration => |decl| {
-                        // check that the variable hasnt already been declared already
                         if (scope.lookup_symbol(decl.name.type.identifier)) |sym| {
                             s.d.emit(.{ .file_id = file_id, .span = d.span }, .err, "Redeclaration of symbol {s}", .{sym.name});
                             break;
                         }
-                        // check
                         if (decl.mut) {
                             if (decl.type == null and decl.val == null) s.d.emit(.{ .file_id = file_id, .span = d.span }, .err, "Mutable declaration {s} needs a type or value", .{decl.name.type.identifier});
                         } else {
@@ -39,6 +38,7 @@ fn check_file_globals(s: *Checker, file_id: u32, ast: Node, scope: *ScopedSymbol
                                 if (v.type == .undefined) s.d.emit(.{ .file_id = file_id, .span = d.span }, .err, "Immutable declarations' initial values cannot be undefined", .{decl.name.type.identifier});
                             } else s.d.emit(.{ .file_id = file_id, .span = d.span }, .err, "Immutable declarations need an initial value", .{decl.name.type.identifier});
                         }
+                        s.global.push_symbol(.{ .name = decl.name.type.identifier, .kind = .@"var", .symbol_type = null, .type = decl.type, .val = decl.val, .mut = decl.mut, .public = decl.pub_, .location = SourceLocation.init(file_id, d.type) });
                     },
                     else => unreachable,
                 }
@@ -46,6 +46,11 @@ fn check_file_globals(s: *Checker, file_id: u32, ast: Node, scope: *ScopedSymbol
         },
         else => unreachable,
     }
+}
+fn check_ast(s: *Checker, file_id: u32, ast: Node) void {
+    _ = s;
+    _ = file_id;
+    _ = ast;
 }
 
 const ScopedSymbolTable = struct {
@@ -80,6 +85,7 @@ const ScopedSymbolTable = struct {
 const SymbolEntry = struct {
     name: []const u8,
     kind: enum { variable, function, @"struct", @"enum", type },
+    symbol_type: ?TypeInfo,
     type: ?*Node,
     val: ?*Node,
     mut: bool = false,
@@ -95,4 +101,4 @@ const Type = enum {
     pointer,
     type,
 };
-const TypeInfo = union(enum) {};
+const TypeInfo = union(Type) { void: null, int: struct { signed: bool, length: u16 }, float: struct { signed: bool, length: u16 }, undefined: void, nullable: struct { subtype: *TypeInfo }, pointer: struct { subtype: *TypeInfo }, type: void };
