951899bc671e34780e2cb60c56b52f874a55b36a
17robots <mdray@duck.com>
2025-11-29 10:27:21 -0500

    just use member for parsing errors too

--- src/main.zig
+++ src/main.zig
@@ -1067,7 +1067,7 @@ const Parser = struct {
                 var members = std.ArrayList(Node).empty;
                 while (s.curr_tok.tok_type != .eof) {
                     if (s.curr_tok.tok_type == .rbrace) break;
-                    members.append(s.allocator, try s.member_basic()) catch |e| @panic(@errorName(e));
+                    members.append(s.allocator, try s.member()) catch |e| @panic(@errorName(e));
                     if (s.curr_tok.tok_type == .rbrace) break;
                     try s.expect(.comma);
                 }
@@ -1374,21 +1374,6 @@ const Parser = struct {
             break :blk s.create_node_ptr(try s.expression(0));
         } else null;
         return node(NodeType{ .member = .{ .names = names, .type = type_, .val = val } }, span_start.fromSpan(s.curr_tok.loc.span));
-    }
-    fn member_basic(s: *Parser) ParserError!Node {
-        const span_start = s.curr_tok.loc.span;
-        var names = std.ArrayList(Node).empty;
-        while (s.curr_tok.tok_type != .eof) {
-            if (s.curr_tok.tok_type == .colon or s.curr_tok.tok_type == .walrus or s.curr_tok.tok_type == .rbrace) break;
-            names.append(s.allocator, try s.identifier()) catch |e| @panic(@errorName(e));
-            if (s.curr_tok.tok_type == .colon or s.curr_tok.tok_type == .walrus or s.curr_tok.tok_type == .rbrace) break;
-            try s.expect(.comma);
-        }
-        const member_type = if (s.curr_tok.tok_type == .colon) blk: {
-            try s.expect(.colon);
-            break :blk s.create_node_ptr(try s.non_literal_expression());
-        } else null;
-        return node(NodeType{ .member_basic = .{ .names = names, .type = member_type } }, span_start.fromSpan(s.curr_tok.loc.span));
     }
     fn unary_expression(s: *Parser) ParserError!Node {
         const span_start = s.curr_tok.loc.span;
