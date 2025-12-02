8459f1b8616fab9cc031489de7cb44cd8d967eb4
17robots <mdray@duck.com>
2025-12-01 20:15:16 -0500

    feat: gonna try a pipeline thing instead

--- src/main3.zig
+++ src/main3.zig
@@ -1,18 +1,11 @@
 const std = @import("std");
-const Lexer = @import("lexer.zig");
-const Parser = @import("parser.zig");
-const file = @import("file.zig");
-const diagnostic = @import("diagnostic.zig");
-const module = @import("module.zig");
-const Compiler = @import("compiler.zig");
+
+const Compiler = struct {};
+const Preferences = struct {};
+const CompilerBuilder = struct {};
 
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
-    var source_manager = file.SourceManager.init(allocator);
-    var diagnostics = diagnostic.DiagnosticEmitter.init(allocator);
-    var module_resolver = module.ModuleResolver.init(allocator, &source_manager, &diagnostics);
-    var compiler = Compiler.init(allocator, &diagnostics, &source_manager, &module_resolver);
-    try compiler.process("example/main", .check);
 }
